a856c7c64e86bd2ead74dc152d6d51487d8a5202
YumiKim <dwkimym93@gmail.com>
2022-11-11 00:15:02 +0900

    💄  header 컴포넌트 css 추가

--- front/components/layout/Header.js
+++ front/components/layout/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import styles from '../../styles/components/Header.module.scss';
 
 const Header = () => {
     return (
-        <div>
+        <header className={styles.header}>
             header
-        </div>
+        </header>
     );
 };
 
