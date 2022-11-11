9b09f68ad0d5dbb13ee5ca2c0ab255b53cf688af
YumiKim <dwkimym93@gmail.com>
2022-11-11 17:52:48 +0900

    Button 컴포넌트 추가

---
+++ front/components/common/Button.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Button.module.scss';
+
+const Button = ({ children, varient = 'primary', size = 32, ...rest }) => {
+    return (
+        <button className={styles.btn} varient={varient} size={size} {...rest}>
+            {children}
+        </button>
+    );
+};
+
+export default Button;
