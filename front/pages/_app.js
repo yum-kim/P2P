8ffd965af8c35e8e95887dd4fa42c7064e60b613
YumiKim <dwkimym93@gmail.com>
2022-11-15 14:13:33 +0900

    ⚡️

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import Head from 'next/head';
 import propTypes from 'prop-types';
-import '../styles/globals.scss';
+import '../styles/global.scss';
 import wrapper from '../store/configureStore';
 
-const Ptop = ({ Component }) => {
+const Ptop = ({ Component, pageProps }) => {
     return (
         <>
             <Head>
                 <meta charSet="utf-8" />
                 <title>P2P</title>
             </Head>
-            <Component />
+            <Component {...pageProps} />
         </>
     )
 }
