0f18066f5f426e528af7e1310d601d0e8ff86ce0
YumiKim <dwkimym93@gmail.com>
2022-11-17 23:06:44 +0900

    login, signup 컴포넌트 구조 추가

---
+++ front/pages/login.js
@@ -0,0 +1,18 @@
+//login
+import React from 'react';
+import Head from 'next/head';
+
+const myPage = () => {
+    return (
+        <>
+            <Head>
+                <title>P2P | login</title>
+            </Head>
+            <div>
+                login
+            </div>
+        </>
+    );
+};
+
+export default myPage;
--- front/pages/signup.js
+++ front/pages/signup.js
@@ -1,1 +1,20 @@
 //회원가입 페이지
+import React from 'react';
+import Head from 'next/head';
+import AppLayout from '../components/layout/AppLayout';
+
+const myPage = () => {
+    return (
+        <>
+            <Head>
+                <title>P2P | 회원가입</title>
+            </Head>
+            <AppLayout>
+                
+
+            </AppLayout>
+        </>
+    );
+};
+
+export default myPage;
