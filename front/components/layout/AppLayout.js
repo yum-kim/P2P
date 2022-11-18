2da958120bc1788483384eea321aa6019ae81490
YumiKim <dwkimym93@gmail.com>
2022-11-18 21:37:13 +0900

    login 되어있지 않을 때 Login 페이지 추가

--- front/components/layout/AppLayout.js
+++ front/components/layout/AppLayout.js
@@ -3,22 +3,31 @@ import Nav from './Nav';
 import Header from './Header';
 import Footer from './Footer';
 import styles from './AppLayout.module.scss';
+import Login from '../../pages/login';
+import { useSelector } from 'react-redux';
 
 const AppLayout = ({ children }) => {
+    const { isLoggedIn } = useSelector((state) => state.user);
+
     return (
         <div className={styles.app}>
-            <Header />
-            <div className={`container ${styles.wrapper}`}>
-                <section className={styles.section}>
-                    <aside className={`lg-only ${styles.aside}`}>
-                        <Nav />
-                    </aside>
-                    <main className={styles.main}>
-                        {children}
-                    </main>
-                </section>
-            </div>
-            <Footer />
+            {isLoggedIn ? 
+                <>
+                    <Header />
+                    <div className={`container ${styles.wrapper}`}>
+                        <section className={styles.section}>
+                            <aside className={`lg-only ${styles.aside}`}>
+                                <Nav />
+                            </aside>
+                            <main className={styles.main}>
+                                {children}
+                            </main>
+                        </section>
+                    </div>
+                    <Footer />
+                </>
+            : <Login /> 
+            }
         </div>
     )
 }
--- front/pages/login.js
+++ front/pages/login.js
@@ -1,18 +1,52 @@
 //login
-import React from 'react';
+import React, { useState } from 'react';
 import Head from 'next/head';
+import styles from './login.module.scss';
+import Input from '../components/common/Input';
+import Button from '../components/common/Button';
+import { useDispatch } from 'react-redux';
+import { loginAction } from '../reducers/user';
 
-const myPage = () => {
+const Login = () => {
+    const [email, setEmail] = useState();
+    const onChangeEmail = (e) => {
+        setEmail(e.target.value);
+    }
+    const [password, setPassword] = useState();
+    const onChangePassword = (e) => {
+        setPassword(e.target.value);
+    }
+    const dispatch = useDispatch();
+    const onClickLogin = () => {
+        dispatch(loginAction({ email, password }));
+    };
+
     return (
         <>
             <Head>
                 <title>P2P | login</title>
             </Head>
-            <div>
-                login
+            <div className={styles.login}>
+                <h2 className={styles.logo}>
+                    <img src='images/test-logo.png' alt='로고' />
+                </h2>
+                <div className={styles.loginWrapper}>
+                    <div className={styles.email}>
+                        <label htmlFor="email">Email</label>
+                        <Input placeholder='ptop@ptop.com' id='email' onChange={onChangeEmail} />
+                    </div>
+                    <div className={styles.password}>
+                        <label htmlFor="password">Password</label>
+                        <Input type='password' id='password' onChange={onChangePassword} />
+                    </div>
+                    <Button size='40' onClick={onClickLogin}>Login</Button>
+                    <Button size='36' varient='outlined'>Sign in with Google</Button>
+                    <div className={styles.divider}></div>
+                    <Button size='36' varient='secondary'>Sign up</Button>
+                </div>
             </div>
         </>
     );
 };
 
-export default myPage;
+export default Login;
