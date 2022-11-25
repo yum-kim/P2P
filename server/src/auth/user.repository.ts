8eb05a3eb4b6f4fe36ad0f47f976257299414e50
jemining <37904137+jemining@users.noreply.github.com>
2022-11-25 17:17:32 +0900

    로그인 추가 & fork 추가

--- server/src/auth/user.repository.ts
+++ server/src/auth/user.repository.ts
@@ -1,13 +1,34 @@
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { CustomRepository } from 'src/typeorm-ex.decorator';
 import { Repository } from 'typeorm';
 import { AuthCredentialDto } from './dto/auth-credential.dto';
 import { User } from './user.entity';
+import * as bcrypt from 'bcryptjs';
 
 @CustomRepository(User)
 export class UserRepository extends Repository<User> {
   async createUser(authCredentialDto: AuthCredentialDto): Promise<any> {
     const { username, password } = authCredentialDto;
-    const user = this.create({ username, password, usercode: '' });
-    return await this.save(user);
+
+    const salt: any = await bcrypt.genSalt();
+    const hashedPassword: any = await bcrypt.hash(password, salt);
+    const user = this.create({
+      username,
+      password: hashedPassword,
+      usercode: '',
+    });
+    try {
+      return await this.save(user);
+    } catch (e) {
+      // unique Error
+      if (e.code === '23505') {
+        throw new ConflictException('Existing username');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
