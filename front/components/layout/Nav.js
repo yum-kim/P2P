e82b25b147c4509d08948916f5bb9bdc9ed3f05d
YumiKim <dwkimym93@gmail.com>
2022-11-29 23:30:55 +0900

    nav active 효과 추가

--- front/components/layout/Nav.js
+++ front/components/layout/Nav.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import Link from "next/link";
 import styles from './Nav.module.scss';
+import { useRouter } from 'next/dist/client/router';
 
 const Nav = () => {
+    const router = useRouter();
+
     return (
         <nav className={styles.nav}>
             <Link href="/">
-                <a>
+                <a className={router.pathname == '/' ? styles.active : ''}>
                     <i className="bi bi-house"></i>
                     Feed
                 </a>
             </Link>
             <Link href="/messenger">
-                <a>
+                <a className={router.pathname == '/messenger' ? styles.active : ''}>
                     <i class="bi bi-chat-left-text"></i>
                     Message 
                 </a>
