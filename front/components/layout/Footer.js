7a312882d6f941e896b0e27b6a57962e4252c8e7
YumiKim <dwkimym93@gmail.com>
2022-11-29 22:54:11 +0900

    footer 추가 / 기타 컴포넌트 여백 수정

--- front/components/layout/Footer.js
+++ front/components/layout/Footer.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import styles from './Footer.module.scss';
 
 const Footer = () => {
     return (
-        <footer>
-            
+        <footer className={styles.footer}>
+            <p>© Copyright 2022. All Rights Reserved.</p>
         </footer>
     );
 };
