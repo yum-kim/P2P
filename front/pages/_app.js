97b0152d126460796424d39772bc17b4114a1608
YumiKim <dwkimym93@gmail.com>
2022-11-30 19:05:25 +0900

    favicon 적용

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -9,6 +9,10 @@ const Ptop = ({ Component, pageProps }) => {
         <>
             <Head>
                 <meta charSet="utf-8" />
+                <link rel="shortcut icon" href="favicon.ico" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png"/>
+                <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png"/>
                 <title>P2P</title>
             </Head>
             <Component {...pageProps} />
