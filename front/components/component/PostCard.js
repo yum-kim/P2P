2a75d6ec06cf2498eaa5d0e8305eda573dcbc666
YumiKim <dwkimym93@gmail.com>
2023-03-14 19:39:13 +0900

    post 공개여부 UI 추가

--- front/components/component/PostCard.js
+++ front/components/component/PostCard.js
@@ -1,18 +1,27 @@
 import React, { useState, useCallback } from 'react';
 import styles from './PostCard.module.scss';
 import Comment from './Comment';
-import { BsHandThumbsUpFill, BsHandThumbsUp, BsChatLeftTextFill, BsChatLeftText } from "react-icons/bs";
+import { BsHandThumbsUpFill, BsHandThumbsUp, BsChatLeftTextFill, BsChatLeftText, BsLockFill, BsUnlock } from "react-icons/bs";
+import Button from '../element/Button/Button';
+import { useDispatch } from 'react-redux';
 
 const PostCard = ({ post }) => {
     const [showComments, setShowComments] = useState(false);
     const [liked, setLiked] = useState(false);
     const { postId, postDate, user, content, imagePath } = post;
+    const dispatch = useDispatch();
+    
     const onToggleLiked = useCallback(() => {
         setLiked((prev) => !prev);
     }, []);
     const onToggleComments = useCallback(() => {
         setShowComments((prev) => !prev);
     }, []);
+
+    //TODO: post status 변경
+    const onChangePostStatus = () => {
+        
+    }
 
     return (
         <div className={styles.card}>
@@ -22,7 +31,13 @@ const PostCard = ({ post }) => {
                 </div>
                 <div>
                     <p className={styles.name}>{user.nickname}</p>
-                    <p className={styles.date}>{postDate}</p>
+                    <div>
+                        <span className={styles.date}>{postDate}</span>
+                        <button className={styles.status} onClick={onChangePostStatus}>
+                            <BsLockFill/>
+                            {/* <BsUnlock /> */}
+                        </button>
+                    </div>
                 </div>
             </div>
             <div className={styles.content}>
