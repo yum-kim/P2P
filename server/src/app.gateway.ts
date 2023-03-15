8faf03a201972f430a113a92e794449bfafaa0ca
jemining <37904137+jemining@users.noreply.github.com>
2023-03-15 23:02:16 +0900

    웹소켓 base 코드

---
+++ server/src/app.gateway.ts
@@ -0,0 +1,35 @@
+import {
+  WebSocketGateway,
+  WebSocketServer,
+  OnGatewayInit,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  SubscribeMessage,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+
+@WebSocketGateway()
+export class AppGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer()
+  server: Server;
+
+  afterInit(server: any) {
+    console.log('Socket server initialized');
+  }
+
+  handleConnection(client: any, ...args: any[]) {
+    console.log(`Client connected: ${client.id}`);
+  }
+
+  handleDisconnect(client: any) {
+    console.log(`Client disconnected: ${client.id}`);
+  }
+
+  @SubscribeMessage('hello')
+  handleMessage(client: Socket, payload: any): string {
+    console.log(`Received message from client: ${payload}`);
+    return 'world';
+  }
+}
