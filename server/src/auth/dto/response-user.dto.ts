142f83ed0a6cc1b663fdb749e86c514d97b1f5ab
jemin-kim <37904137+jemining@users.noreply.github.com>
2023-06-11 16:26:48 +0900

    fix: entity 변경에 따른 userPayload 변경 (#1)

--- server/src/auth/dto/response-user.dto.ts
+++ server/src/auth/dto/response-user.dto.ts
@@ -2,7 +2,7 @@ import { ApiProperty } from '@nestjs/swagger';
 
 export class ResponseUserDto {
   @ApiProperty({ description: 'userID' })
-  userid: number;
+  id: number;
 
   @ApiProperty({ description: 'AccessToken' })
   accessToken: string;
