d91378871152d54960264d534343081057293cbe
jemining <37904137+jemining@users.noreply.github.com>
2023-06-18 18:25:39 +0900

    env 제외 및 config 수정

--- server/src/config/typeorm.config.ts
+++ server/src/config/typeorm.config.ts
@@ -8,10 +8,10 @@ const dbConfig = config.get('db');
 export const typeORMConfig: TypeOrmModuleOptions = {
   type: dbConfig.type,
   host: process.env.RDS_HOSTNAME, // [AWS RDB 용]process.env.RDS_HOSTNAME
-  port: process.env.RDS_PORT as number, // [AWS RDB 용]process.env.RDS_PORT
+  port: Number(process.env.RDS_PORT), // [AWS RDB 용]process.env.RDS_PORT
   username: process.env.RDS_USERNAME, // [AWS RDB 용]process.env.RDS_USERNAME
-  password: dbConfig.password, // [AWS RDB 용]process.env.RDS_PASSWORD
-  database: dbConfig.database, // [AWS RDB 용]process.env.RDS_DB_NAME
+  password: process.env.RDS_PASSWORD, // [AWS RDB 용]process.env.RDS_PASSWORD
+  database: process.env.RDS_DB_NAME, // [AWS RDB 용]process.env.RDS_DB_NAME
   entities: [__dirname + '/../**/*.entity.{js,ts}', Board],
   namingStrategy: new SnakeNamingStrategy(),
   logging: true,
