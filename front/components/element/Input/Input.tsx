3aca72408e26f5206205e232708cf15588998162
YumiKim <dwkimym93@gmail.com>
2023-06-26 21:29:38 +0900

    input props 추가

--- front/components/element/Input/Input.tsx
+++ front/components/element/Input/Input.tsx
@@ -2,26 +2,32 @@ import React, { forwardRef, ReactNode } from 'react';
 import styles from './Input.module.scss';
 
 interface IInputProps {
+    id: string,
     type: 'text' | 'textarea' | 'password',
+    placeholder: string,
     variant: 'primary' | 'secondary' | 'background',
     value: string,
     height?: string,
     onChange: (e:React.ChangeEvent) => void,
 }
 
-const Input = forwardRef<HTMLInputElement | HTMLTextAreaElement, IInputProps>(({ type, variant, value, height, onChange }, ref) => {
+const Input = forwardRef<HTMLInputElement | HTMLTextAreaElement, IInputProps>(({ id, type, placeholder, variant, value, height, onChange }, ref) => {
     return (
         <>
             {type == 'textarea' ?
                 <textarea
+                    id={id}
                     ref={ref as React.RefObject<HTMLTextAreaElement>}
+                    placeholder={placeholder}
                     className={`${styles.textarea} ${styles['variant-' + variant]} ${styles['height-' + height]}`}
                     value={value}
                     onChange={onChange}
                 >
                 </textarea>
                 : <input
+                    id={id}
                     ref={ref as React.RefObject<HTMLInputElement>}
+                    placeholder={placeholder}
                     type={type}
                     className={`${styles.input} ${styles['variant-' + variant]}`}
                     value={value}
