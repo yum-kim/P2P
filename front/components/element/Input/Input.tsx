73c424b3a820d341dc9a7c2574aa7866bf2d825c
YumiKim <dwkimym93@gmail.com>
2023-06-26 22:41:07 +0900

    input props 추가

--- front/components/element/Input/Input.tsx
+++ front/components/element/Input/Input.tsx
@@ -1,17 +1,19 @@
-import React, { forwardRef, ReactNode } from 'react';
+import React, { forwardRef } from 'react';
 import styles from './Input.module.scss';
 
 interface IInputProps {
-    id: string,
+    id?: string,
     type: 'text' | 'textarea' | 'password',
-    placeholder: string,
-    variant: 'primary' | 'secondary' | 'background',
-    value: string,
+    placeholder?: string,
+    variant?: 'primary' | 'secondary' | 'background',
+    value?: string,
     height?: string,
-    onChange: (e:React.ChangeEvent) => void,
+    onChange?: (e:React.ChangeEvent) => void,
+    disabled?: boolean,
 }
 
-const Input = forwardRef<HTMLInputElement | HTMLTextAreaElement, IInputProps>(({ id, type, placeholder, variant, value, height, onChange }, ref) => {
+const Input = forwardRef<HTMLInputElement | HTMLTextAreaElement, IInputProps>((
+    { id, type, placeholder, variant, value, height, onChange, disabled }, ref) => {
     return (
         <>
             {type == 'textarea' ?
@@ -22,6 +24,7 @@ const Input = forwardRef<HTMLInputElement | HTMLTextAreaElement, IInputProps>(({
                     className={`${styles.textarea} ${styles['variant-' + variant]} ${styles['height-' + height]}`}
                     value={value}
                     onChange={onChange}
+                    disabled={disabled}
                 >
                 </textarea>
                 : <input
@@ -32,6 +35,7 @@ const Input = forwardRef<HTMLInputElement | HTMLTextAreaElement, IInputProps>(({
                     className={`${styles.input} ${styles['variant-' + variant]}`}
                     value={value}
                     onChange={onChange}
+                    disabled={disabled}
                 />
             }
         </>
