177b22a8f8e88330dc3cbdaf2a22398f0159c618
YumiKim <dwkimym93@gmail.com>
2023-07-11 16:25:16 +0900

    https -> http 요청 시 mixed content 오류 수정

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -18,10 +18,11 @@ const App: React.FC<AppProps> = ({ Component }) => {
         <>
             <Head>
                 <meta charSet="utf-8" />
+                <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
                 <link rel="shortcut icon" href="favicon.ico" />
                 <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
                 <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png"/>
-                <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png"/>
+                <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
                 <title>P2P</title>
             </Head>
             <Provider store={store}>
