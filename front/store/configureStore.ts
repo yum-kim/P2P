014a138ea3d786419e8d026c32c3e2a4eb997cf7
YumiKim <dwkimym93@gmail.com>
2023-07-13 20:44:18 +0900

    useSocket 주석, redux내로 socket 관리 변경

--- front/store/configureStore.ts
+++ front/store/configureStore.ts
@@ -2,6 +2,8 @@ import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
 import createSagaMiiddleWare from 'redux-saga';
 import authReducer from './slices/auth';
 import postReducer from './slices/post';
+import chatReducer from './slices/chat';
+import socketReducer from './slices/socket';
 import rootSaga from './sagas/index';
 
 const sagaMiddleware = createSagaMiiddleWare();
@@ -10,6 +12,8 @@ export const store = configureStore({
     reducer: {
         auth: authReducer,
         post: postReducer,
+        chat: chatReducer,
+        socket: socketReducer,
     },
     middleware: [...getDefaultMiddleware({ thunk: false, serializableCheck: false }), sagaMiddleware],
 });
