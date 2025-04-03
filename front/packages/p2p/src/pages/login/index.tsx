2a666f9cebe64cb410023da1c639b52eb68586b4
YumiKim <dwkimym93@gmail.com>
2025-04-03 16:54:21 +0900

    p2p 기존 파일 src 폴더 내로 이동

---
+++ front/packages/p2p/src/pages/login/index.tsx
@@ -0,0 +1,144 @@
+//login
+import React, { useState, useEffect, useCallback } from 'react';
+import Head from 'next/head';
+import styles from './login.module.scss';
+import Input from '../../components/element/Input/Input';
+import Button from '../../components/element/Button/Button';
+import { useDispatch, useSelector } from 'react-redux';
+import Link from 'next/link';
+import { useRouter } from 'next/dist/client/router';
+import Loading from '../../components/common/Loading/Loading';
+import useModal from '../../hooks/useModal';
+import { RootState } from '../../store/configureStore';
+import {
+  logInRequest,
+  issueAccessTokenRequest,
+  resetSpecificAuth,
+  resetAllAuthError,
+  resetAllAuth,
+} from '../../store/slices/auth';
+import useInput from '../../hooks/useInput';
+import { getCookie, TOKEN_COOKIE_NAME } from '../../utils/cookie';
+import { resetAllPostError } from '../../store/slices/post';
+import { disconnetSocketRequest } from '../../store/slices/socket';
+
+const Login = () => {
+  const [username, onChangeUsername] = useInput('');
+  const [password, onChangePassword] = useInput('');
+  const dispatch = useDispatch();
+  const router = useRouter();
+  const {
+    logInLoading,
+    logInError,
+    logOutDone,
+    expireRefreshTokenError,
+    user,
+    issueAccessTokenLoading,
+    removeAccountDone,
+  } = useSelector((state: RootState) => state.auth);
+  const { socket } = useSelector((state: RootState) => state.socket);
+  const { Modal, onShowModal } = useModal(false);
+
+  const onSubmitLogin = useCallback(
+    async (e: React.ChangeEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      if (!username || !password) {
+        onShowModal('빈 값이 있습니다.');
+        return;
+      }
+
+      dispatch(logInRequest({ username, password }));
+    },
+    [username, password],
+  );
+
+  useEffect(() => {
+    if (expireRefreshTokenError) {
+      onShowModal('토큰이 만료되어 권한이 없습니다.\n다시 로그인 후 이용해주세요.', {
+        cancel: () => {
+          dispatch(resetAllAuth());
+          dispatch(resetAllPostError());
+        },
+      });
+    }
+  }, [expireRefreshTokenError]);
+
+  useEffect(() => {
+    logInError &&
+      onShowModal(logInError.message, {
+        cancel: () => {
+          dispatch(resetSpecificAuth('logInError'));
+        },
+      });
+  }, [logInError]);
+
+  useEffect(() => {
+    !logOutDone && user && router.push('/feed');
+  }, [user, logOutDone]);
+
+  useEffect(() => {
+    if (!expireRefreshTokenError && !removeAccountDone && logOutDone && !user) {
+      onShowModal('로그아웃이 완료되었습니다.', {
+        cancel: () => {
+          dispatch(resetSpecificAuth('logOutDone'));
+        },
+      });
+    }
+  }, [logOutDone]);
+
+  useEffect(() => {
+    if (removeAccountDone) {
+      onShowModal('회원탈퇴로 로그아웃 처리 되었습니다.', {
+        cancel: () => {
+          dispatch(resetSpecificAuth('removeAccountDone'));
+        },
+      });
+    }
+  }, [removeAccountDone]);
+
+  useEffect(() => {
+    const refreshToken = getCookie(TOKEN_COOKIE_NAME);
+    refreshToken && dispatch(issueAccessTokenRequest());
+    socket && dispatch(disconnetSocketRequest());
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>P2P | login</title>
+      </Head>
+      {logInLoading || (issueAccessTokenLoading && <Loading />)}
+      <Modal />
+
+      <section className={styles.login}>
+        <h2 className={styles.logo}>
+          <img src="images/logo.svg" alt="로고" />
+        </h2>
+        <div className={styles.loginWrapper}>
+          <form onSubmit={onSubmitLogin}>
+            <div className={styles.username}>
+              <label htmlFor="id">Username</label>
+              <Input type="text" placeholder="username" id="id" onChange={onChangeUsername} />
+            </div>
+            <div className={styles.password}>
+              <label htmlFor="password">Password</label>
+              <Input type="password" placeholder="password" id="password" onChange={onChangePassword} />
+            </div>
+            <Button type="submit" size="40">
+              Login
+            </Button>
+          </form>
+          {/* <Button size='36' variant='outlined'>Sign in with Google</Button> */}
+          <div className={styles.divider}></div>
+          <Link href="/signup">
+            <Button size="36" variant="secondary">
+              Sign up
+            </Button>
+          </Link>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Login;
