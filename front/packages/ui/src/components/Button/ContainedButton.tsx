c37ae254eccfbc2ac57af8dac9ceb150e9821972
YumiKim <dwkimym93@gmail.com>
2025-08-08 15:26:35 +0900

    fix: Button 컴포넌트 color 값 추출 리팩토링

--- front/packages/ui/src/components/Button/ContainedButton.tsx
+++ front/packages/ui/src/components/Button/ContainedButton.tsx
@@ -2,6 +2,19 @@ import clsx from "clsx";
 import { Spinner } from "../Spinner/Spinner";
 import { ButtonProps } from "./Button.type";
 
+const colorThemes = {
+  purple: {
+    bg: "bg-p2p-purple",
+    text: "text-p2p-background",
+    hoverBg: "hover:bg-p2p-purple-dark",
+  },
+  black: {
+    bg: "bg-p2p-black",
+    text: "text-p2p-white",
+    hoverBg: "hover:bg-p2p-dark",
+  },
+};
+
 export const ContainedButton = ({
   children,
   color = "purple",
@@ -10,22 +23,21 @@ export const ContainedButton = ({
   className,
   ...props
 }: ButtonProps) => {
-  const themeBgColor = color === "purple" ? "bg-p2p-purple" : "bg-p2p-white";
-  const themeTextColor =
-    color === "purple" ? "text-p2p-background" : "text-p2p-black";
+  const theme = colorThemes[color];
 
   return (
     <button
       disabled={loading || disabled}
       className={clsx(
         "flex gap-x-[10px] bg-p2p- text-p2p-16 items-center rounded-md px-[10px] py-[8px] justify-center hover:bg-p2p-purple-dark hover:transition-colors hover:duration-100 disabled:bg-p2p-secondary",
-        themeBgColor,
+        theme.bg,
+        theme.hoverBg,
         className
       )}
       {...props}
     >
       <div>{loading && <Spinner />}</div>
-      <div className={themeTextColor}>{children}</div>
+      <div className={theme.text}>{children}</div>
     </button>
   );
 };
--- front/packages/ui/src/components/Button/OutlinedButton.tsx
+++ front/packages/ui/src/components/Button/OutlinedButton.tsx
@@ -2,6 +2,17 @@ import clsx from "clsx";
 import { Spinner } from "../Spinner/Spinner";
 import { ButtonProps } from "./Button.type";
 
+const colorThemes = {
+  purple: {
+    text: "text-p2p-purple",
+    outline: "outline-p2p-purple focus:outline-p2p-purple",
+  },
+  black: {
+    text: "text-p2p-black",
+    outline: "outline-p2p-black focus:outline-p2p-black",
+  },
+};
+
 export const OutlinedButton = ({
   children,
   color = "purple",
@@ -10,24 +21,19 @@ export const OutlinedButton = ({
   className,
   ...props
 }: ButtonProps) => {
-  const textColor = color === "purple" ? "text-p2p-purple" : "text-p2p-black";
-  const outlineColor =
-    color === "purple"
-      ? "outline-p2p-purple focus:outline-p2p-purple"
-      : "outline-p2p-black focus:outline-p2p-black";
-
+  const theme = colorThemes[color];
   return (
     <button
       disabled={loading || disabled}
       className={clsx(
-        "flex gap-x-[10px] text-p2p-16 items-center rounded-md px-[10px] py-[8px] justify-center disabled:bg-p2p-secondary bg-p2p-white outline-1 outline focus:outline focus:outline-1",
-        outlineColor,
+        "flex gap-x-[10px] text-p2p-16 items-center rounded-md px-[10px] py-[8px] justify-center disabled:bg-p2p-secondary bg-p2p-white outline-1 outline focus:outline focus:outline-1 transition duration-100",
+        theme.outline,
         className
       )}
       {...props}
     >
       <div>{loading && <Spinner />}</div>
-      <div className={textColor}>{children}</div>
+      <div className={theme.text}>{children}</div>
     </button>
   );
 };
