2085d0223949ce322c681ae68a30afa98282eeec
YumiKim <dwkimym93@gmail.com>
2025-08-10 13:52:27 +0900

    add: Footer UI 추가

--- front/packages/p2p-v2/src/components/Footer.tsx
+++ front/packages/p2p-v2/src/components/Footer.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 export const Footer = () => {
   return (
-    <div className="w-full flex items-center justify-center">
-      <div className="w-full max-w-screen-p2p-lg py-[30px] px-[12px] p2p-sm:px-[24px] p2p-md:px-[32px] p2p-lg:px-0">
-        footer
+    <div className="w-full flex items-center justify-center border-t border-p2p-border">
+      <div className="w-full max-w-screen-p2p-lg py-[30px] px-[12px] p2p-sm:px-[24px] p2p-md:px-[32px] p2p-lg:px-0 flex items-center justify-center text-p2p-12 text-p2p-primary">
+        © Copyright 2022. All Rights Reserved.
       </div>
     </div>
   );
